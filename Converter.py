49c1f7d3ff4489664e4574d41c88b66d91a8fec0
olkoro <olkoro@ttu.ee>
2019-04-13 00:44:50 +0300

    initial

--- Converter.py
+++ Converter.py
@@ -3,7 +3,9 @@ from tkinter import *
 from tkinter import ttk
 from collections import namedtuple
 
-def calculate(*args):
+p1 = Proj(init='epsg:3301', preserve_units=False)
+
+def calculate():
     label()
     try:
         x = float(xInput.get())
@@ -14,60 +16,29 @@ def calculate(*args):
         y = yInput.get()
         (y, x) = wgs_to_est(x, y)
     answer0.set(x)
-    answer.set(y)
+    answer1.set(y)
 def wgs_to_est(x,y):
     xdeg = float(x.split('°')[0])
     xmin = float(x.split('°')[1].split("'")[0])
     xsek = float(x.split("'")[1].split('"')[0])
-    print(xdeg, xmin, xsek)
     ydeg = float(y.split('°')[0])
     ymin = float(y.split('°')[1].split("'")[0])
     ysek = float(y.split("'")[1].split('"')[0])
-    print(ydeg, ymin, ysek)
-
-    x,y = convert_degrees_to_decimal(xdeg, xmin, xsek), convert_degrees_to_decimal(ydeg, ymin, ysek)
-    print(x, y)
-    p1 = Proj(init='epsg:3301', preserve_units=False)
+    x,y = degrees_to_decimal(xdeg, xmin, xsek), degrees_to_decimal(ydeg, ymin, ysek)
     y,x = p1(y,x)
-    #y, x = round(y, 1), round(x, 1)
     return y,x
 def est_to_wgs(x,y):
-    p1 = Proj(init='epsg:3301', preserve_units=False)
     (x, y) = p1(y, x, inverse=True)
-    return convert_decimal_to_degrees(x), convert_decimal_to_degrees(y)
+    return decimal_to_degrees(x), decimal_to_degrees(y)
 
-def convert_decimal_to_degrees(decimal):
-    # type: (float) -> Tuple[int, int, float]
-    """
-    Convert decimal degrees to degree-minute-second format.
-
-    Example:
-        >>> convert_decimal_to_degrees(30.56)
-        (30, 33, 36)
-
-    Args:
-        Decimal: degree in decimals
-    Returns:
-        Tuple of degree-minute-second.
-    """
+def decimal_to_degrees(decimal):
     degrees = int(decimal)
     minutes = int((decimal - degrees) * 60)
     seconds = round((decimal - degrees - minutes / 60.) * 3600, 4)
     #return degrees, minutes, seconds
     return "".join(map(str, [degrees, '° ', minutes, "' ", seconds, '"']))
 
-def convert_degrees_to_decimal(degrees, minutes, seconds):
-    # type: (int, int, float) -> float
-    """
-    Convert degree-minute-second format to decimal degrees.
-
-    Example:
-    >>> convert_degrees_to_decimal(10, 23, 42)
-    10.395
-
-    Returns:
-         Degrees in decimal format
-    """
+def degrees_to_decimal(degrees, minutes, seconds):
     return degrees + minutes / 60. + seconds / 3600.
 
 def swap():
@@ -80,10 +51,10 @@ def swap():
     entry1.delete(0,'end')
     entry1.insert(0,answer0.get())
     entry2.delete(0,'end')
-    entry2.insert(0,answer.get())
+    entry2.insert(0, answer1.get())
 
     answer0.__del__()
-    answer.__del__()
+    answer1.__del__()
 
     label()
     return
@@ -111,7 +82,7 @@ root.rowconfigure(0, weight=1)
 
 xInput = StringVar()
 yInput = StringVar()
-answer = StringVar()
+answer1 = StringVar()
 answer0 = StringVar()
 unit1 = StringVar()
 unit2 = StringVar()
@@ -121,7 +92,7 @@ entry1.grid(column=1, row=1, sticky=(W, E))
 entry2 = ttk.Entry(mainframe, width=7, textvariable=yInput)
 entry2.grid(column=2, row=1, sticky=(W, E))
 
-ttk.Label(mainframe, textvariable=answer).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(mainframe, textvariable=answer1).grid(column=2, row=2, sticky=(W, E))
 ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=1, row=3, sticky=W)
 
 ttk.Button(mainframe, text="Swap", command=swap).grid(column=4, row=3, sticky=W)
