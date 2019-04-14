4c75d4bef3abcb23d42f04345b18a410768a5c11
olkoro <olehik98@gmail.com>
2019-04-14 14:30:49 +0300

    documentation

--- Converter.py
+++ Converter.py
@@ -1,3 +1,4 @@
+"""Coordinates conversion from from WGS84 coordinates to L-Est97 and vice versa."""
 from pyproj import Proj
 from tkinter import *
 from tkinter import ttk
@@ -5,6 +6,10 @@ from tkinter import ttk
 p1 = Proj(init='epsg:3301', preserve_units=False)
 
 def calculate():
+    """
+    Calls conversion functions and writes the answer into the GUI
+    :return:
+    """
     try:
         x = float(xInput.get())
         y = float(yInput.get())
@@ -17,6 +22,12 @@ def calculate():
     answer2.set(y)
 
 def wgs_to_est(x,y):
+    """
+    Converts from WGS84 to L-Est97
+    :param x: WGS84 latitude to convert
+    :param y: WGS84 longitude to convert
+    :return: coordinates in L-Est97
+    """
     xdeg = float(x.split('°')[0])
     xmin = float(x.split('°')[1].split("'")[0])
     xsek = float(x.split("'")[1].split('"')[0])
@@ -28,29 +39,52 @@ def wgs_to_est(x,y):
     return y, x
 
 def est_to_wgs(x,y):
+    """
+    Converts from L-Est97 to WGS84
+    :param x: L-Est97 X-axe direction is to North
+    :param y: L-Est97 Y-axe direction is to East
+    :return: coordinates in WGS84
+    """
     (x, y) = p1(y, x, inverse=True)
     return decimal_to_degrees(x), decimal_to_degrees(y)
 
 def decimal_to_degrees(decimal):
+    """
+    Converts decimal numbers to degrees, minutes and seconds and formats it with appropriate symbols
+    :param decimal: Coordinate in decimal number
+    :return: Coordinate in degrees, minutes and seconds
+    """
     degrees = int(decimal)
     minutes = int((decimal - degrees) * 60)
     seconds = round((decimal - degrees - minutes / 60.) * 3600, 4)
     return "".join(map(str, [degrees, '° ', minutes, "' ", seconds, '"']))
 
 def degrees_to_decimal(degrees, minutes, seconds):
+    """
+    Converts degrees, minutes and seconds to decimal numbers
+    :param degrees:
+    :param minutes:
+    :param seconds:
+    :return: Decimal number
+    """
     return degrees + minutes / 60. + seconds / 3600.
 
+
 def swap():
+    """
+    Changes conversion modes between "WGS84 to L-Est97" and "L-Est97 to WGS84" and displays it in GUI
+    :return:
+    """
     v = unit1.get()
     unit1.set(unit2.get())
     unit2.set(v)
-    entry1.delete(0,'end')
-    entry2.delete(0,'end')
+    entry1.delete(0, 'end')
+    entry2.delete(0, 'end')
     entry1.insert(0, answer1.get())
     entry2.insert(0, answer2.get())
     answer1.__del__()
     answer2.__del__()
-    return
+
 
 root = Tk()
 root.title("Python Advanced HW1")
@@ -66,12 +100,12 @@ unit1 = StringVar()
 unit2 = StringVar()
 unit1.set("WGS84")
 unit2.set("Est97")
-entry1 = ttk.Entry(mainframe, width=7, textvariable=xInput)
+entry1 = ttk.Entry(mainframe, width=17, textvariable=xInput)
 entry1.grid(column=1, row=1, sticky=(W, E))
-entry2 = ttk.Entry(mainframe, width=7, textvariable=yInput)
+entry2 = ttk.Entry(mainframe, width=17, textvariable=yInput)
 entry2.grid(column=2, row=1, sticky=(W, E))
 ttk.Label(mainframe, textvariable=answer2).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=1, row=3, sticky=W)
+ttk.Button(mainframe, text="Convert", command=calculate).grid(column=1, row=3, sticky=W)
 ttk.Button(mainframe, text="Swap", command=swap).grid(column=4, row=3, sticky=W)
 ttk.Label(mainframe, text="in").grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, textvariable=answer1).grid(column=1, row=2, sticky=E)
